141f19e9fd244edb8d5ed7c70a3a545cd7d4caaf
Ernesto <root@jugueteriadelicias3.com.ar>
2018-06-07 02:13:03 +0000

    change code

--- barney.php
+++ barney.php
@@ -11,9 +11,9 @@
  <fieldset>
   <legend><b>Elija el host de la victima:</b></legend>
          <select name="host">
- 		<option value="ninguno">Elija un host</option>
+ 		<option value="ninguno">Ninguno</option>
  		<option value="192.241.163.11">192.241.163.11 [Barney]</option>
-  		<option value="159.203.93.192">138.68.40.240 [Empresa X]</option>
+  		<option value="138.68.40.240">138.68.40.240 [Empresa X]</option>
                 </select>
         <input type="submit" value="Scan Port!">
  </fieldset>
@@ -25,24 +25,26 @@
 			# ejecutar nmap y guardarlo en una variable
 			$scan1 = shell_exec('nmap ' . $_POST['host'] . ' -p 21,20 | grep open');
 			$scan2 = shell_exec('nmap ' . $_POST['host'] . ' -p 110 | grep open');
+			$scan3 = shell_exec('nmap ' . $_POST['host'] . ' -p 80 | grep open');
 
 			# Si la variable esta vacia el puerto esta cerrado, caso contrario esta abierto
-			if (empty($scan1 or $scan2)){
-				echo "<font color='red'> >>El puerto 21 o 110 no se encuentran abierto...</font>";
-			} elseif ($host == "ninguno"){
+			if (empty($scan1 or $scan2 or $scan3)){
+				echo "<font color='red'> >>El puerto 21, 110 u 80 no se encuentran abiertos...</font>";
+			} elseif ($_POST['host'] == "ninguno"){
 				echo "No ha seleccionado ningún host...";
 			} else {
 				echo "<font color='lime'>";
 				echo "PORT   STATE  SERVICE<br>";
 				echo $scan1 . "<br>";
 				echo $scan2 . "<br>";
+				echo $scan3 . "<br>";
 				echo "</font>";
 			}
 		}
 
 	scan_nmap();
 	?>
-<p><b>Nota:</b> Esto es una prueba educativa</p>
+<p><b>Nota:</b> Esto es una prueba educativa con servidores propios del profesor - Ernesto Sequeira</p>
 
 <div align="center"><a href="https://github.com/ernestosequeira/barney/">Github</a></div>
 
